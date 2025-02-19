a973d054c6bed428b32adfa640128945e4d99032
Sushan Tamang <sushantmg2004@gmail.com>
2025-02-19 03:23:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,44 @@
+"use strict"; //treat all js code as a newer version
+
+//alert(1+2) // we are using nodejs not browser
+
+console.log(3
+    +
+    3)// code readability should be high
+    
+    
+console.log("Sushan")
+
+let name ="Sushan"
+let age = 20
+let isLoggedIn = true
+let state = null
+let country;
+
+
+// number => 2 to the power 53 (range in memory address) // It is a number
+// bigInt //used in big numbers (for instance :trading,facebook ,big companies etc) It is a function
+// string=>" "
+// boolean=> true/false // It is a function
+// null=>standelone value // It is an object
+// undefined=> // It is an undefined
+// symbol=> unique // It is a function
+
+// object
+
+console.log(typeof "Sushan")
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof 12)
+console.log(typeof BigInt)
+console.log(typeof Symbol)
+console.log(typeof Boolean)
+
+
+
+
+
+
+
+    
+
