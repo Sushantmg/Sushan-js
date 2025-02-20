8cf6c76c367a9ac68a9e8b6f361748a8a2121c2b
Sushan Tamang <sushantmg2004@gmail.com>
2025-02-20 16:00:05 +0000

    comparison

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -3,5 +3,11 @@ console.log(2 >= 1)
 console.log( 2==2)
 console.log(2 != 3) // not equals 
 
+console.log( null > 0) // false
+console.log( null == 0) //false (equalliaty)
+console.log( null >= 0) // true (comparison)
+
+
+
 
 
