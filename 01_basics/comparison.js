4de2907e8eeb995bdbc60bfec4f691f6106538e4
Sushan Tamang <sushantmg2004@gmail.com>
2025-02-20 16:04:48 +0000

    comparison

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -4,8 +4,12 @@ console.log( 2==2)
 console.log(2 != 3) // not equals 
 
 console.log( null > 0) // false
-console.log( null == 0) //false (equalliaty)
+console.log( null == 0) //false (equality)
 console.log( null >= 0) // true (comparison)
+
+//strict check
+console.log( "2" === 2) // checks datatypes (false)
+
 
 
 
