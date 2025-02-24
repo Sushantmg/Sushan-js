add3c16087f1193ce4f84672e54cc8e5ad18b840
Sushan Tamang <sushantmg2004@gmail.com>
2025-02-24 10:15:10 +0000

    datatypes summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -8,3 +8,20 @@ const num = 1
 const id = Symbol("123")
 const anotherID = Symbol("123")
 console.log(id === anotherID)
+
+
+
+// Non-primitve ( reference)
+// array , objects, functions
+
+const heros = ["SpiderMan","BatMan","WonderWoman"]; // array
+
+
+let myObj = {
+    F_name:"Sushan",age: 20
+}
+
+const myFunction = function()
+{
+    console.log("Hello World")   
+}
