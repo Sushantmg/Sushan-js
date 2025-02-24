bac0f7f916fa5c8bbf97f61028a687b3a0998273
Sushan Tamang <sushantmg2004@gmail.com>
2025-02-24 13:14:24 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -3,4 +3,12 @@ const repoCount= 7
 
 console.log('My name is ${name} and my repoCount is ${repCount}'); // My name is Sushan and my repoCount is 7
 
+const gameName = new String ("lalakta")
+
+console.log(gameName[1]);// a
+console.log(gameName.__proto__)// {..}
+console.log(gameName.toUpperCase());//LALAKTa
+console.log(gameName.length);//7
+console.log(gameName.charAt(1));//a
+console.log(gameName.indexOf('t'));//5
 
