68524a4da78578a1ffdf92c9dfc60319c795e6d0
Sushan Tamang <sushantmg2004@gmail.com>
2025-02-24 13:58:57 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -7,8 +7,25 @@ const gameName = new String ("lalakta")
 
 console.log(gameName[1]);// a
 console.log(gameName.__proto__)// {..}
-console.log(gameName.toUpperCase());//LALAKTa
+console.log(gameName.toUpperCase());//LALAKTA
 console.log(gameName.length);//7
 console.log(gameName.charAt(1));//a
 console.log(gameName.indexOf('t'));//5
+
+
+// go through string methods throughly all at mdn docs
+
+const newString = gameName.substring(0,4)
+console.log(newString); //lala
+
+const anotherString = gameName.slice(-7,4)
+console.log(anotherString);//lala
+
+const newStringOne = "     Sushan     "
+console.log(newStringOne);//   Sushan
+console.log(newStringOne.trim());//Sushan
+
+
+const url = "https://Sushan.com/Sushan%20tamang"
+console.log(url.replace('%20','%10'));//"https://Sushan.com/Sushan%10tamang"
 
